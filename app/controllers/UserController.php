34f944cbdcec5ab220f020b7e8f67b7b5ea19655
quandoan21-legion <qdoan21.legion@gmail.com>
2024-10-05 09:46:21 +0700

    Change the get data from  to REACT API JSON

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -16,20 +16,21 @@ class UserController extends BaseController
         if ($_SERVER["REQUEST_METHOD"] === "GET") {
             $this->view('register');
         } else {
-            $this->__instanceUser->setUserName($_REQUEST["username"]);
+            $input = json_decode(file_get_contents('php://input'), true);
+            $this->__instanceUser->setUserName($input["username"]);
             // $this->viewData(!empty($this->__instanceModel->checkUserExist($this->__instanceUser)));
             // die();
             if (!empty($this->__instanceModel->checkUserExist($this->__instanceUser))) {
                 $this->FactoryMessage("error", "User name already exist");
                 $this->view('register');
             } else {
-                $this->__instanceUser->setPassword($_REQUEST["password"]);
-                $this->__instanceUser->setFirstName($_REQUEST['firstname']);
-                $this->__instanceUser->setLastName($_REQUEST['lastname']);
-                $this->__instanceUser->setDob($_REQUEST['dob']);
-                $this->__instanceUser->setPhone($_REQUEST['phone']);
-                $this->__instanceUser->setAddress($_REQUEST['address']);
-                $this->__instanceUser->setBuyerImage($_REQUEST['image']);
+                $this->__instanceUser->setPassword($input["password"]);
+                $this->__instanceUser->setFirstName($input['firstname']);
+                $this->__instanceUser->setLastName($input['lastname']);
+                $this->__instanceUser->setDob($input['dob']);
+                $this->__instanceUser->setPhone($input['phone']);
+                $this->__instanceUser->setAddress($input['address']);
+                $this->__instanceUser->setBuyerImage($input['image']);
                 $this->__instanceModel->createNewUser($this->__instanceUser);
                 // $this->view('register');
                 $this->FactoryMessage("success", "Account Created");
@@ -43,19 +44,19 @@ class UserController extends BaseController
         if ($_SERVER['REQUEST_METHOD'] == "GET") {
             $this->view("login");
         } else if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
-            $this->__instanceUser->setUserName($_REQUEST["username"]);
-            $this->__instanceUser->setPassword($_REQUEST["password"]);
+            $input = json_decode(file_get_contents('php://input'), true);
+            $this->__instanceUser->setUserName($input["username"]);
+            $this->__instanceUser->setPassword($input["password"]);
             $data = $this->__instanceModel->checkUserExist($this->__instanceUser);
 
             if ($data != null && $data["buyerId"] != null) {
                 $_SESSION["username"] = $this->__instanceUser->getUserName();
                 $this->FactoryMessage("success", "Login successfully", $data);
-                $this->view('login');
+                // $this->view('login');
                 echo "exist";
             } else {
                 $this->FactoryMessage("Error", "Login Failed", $data);
-                $this->view("login");
+                // $this->view("login");
             }
         }
     }
@@ -64,11 +65,11 @@ class UserController extends BaseController
         if (isset($_SESSION['username'])) {
             $_SESSION['username'] = null;
             session_destroy();
-            echo "logout success fully";
-            $this->view('login');
+            $this->FactoryMessage("Info", "logout success fully");
+            // $this->view('login');
         } else {
-            $this->view('login');
-            echo "not login yet to logout";
+            // $this->view('login');
+            $this->FactoryMessage("Info", "not login yet to logout");
         }
     }
 
@@ -87,11 +88,11 @@ class UserController extends BaseController
             $this->__instanceUser->setID($result['user_id']);
             $aUserData = $this->__instanceModel->getUserData($this->__instanceUser);
             if ($aUserData != false) {
-                $_SESSION["data"] = $aUserData;
-                $this->viewData($_SESSION);
-                echo "User data above";
+                // $_SESSION["data"] = $aUserData;
+                $this->FactoryMessage("info", "Get User Info Successfully", $aUserData);
+                // $this->viewData($_SESSION);
             } else {
-                echo "User data not exist";
+                $this->FactoryMessage("info", "User data not exist");
             }
         } else if ($_SERVER["REQUEST_METHOD"] === "PUT") {
         }
--- index.php
+++ index.php
@@ -5,4 +5,7 @@ include_once "./app/controllers/BaseController.php";
 include_once "./app/controllers/ValidateController.php";
 include_once "./app/App.php";
 
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 $app = new App($conn);
